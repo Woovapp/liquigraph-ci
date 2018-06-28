aa77e57251576a56c2bb0e6b9e3bdf245a0c25cd
Alex Vakhmenin <alex@woovapp.com>
2018-06-28 15:58:08 +0200

    Smarter migrate script

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ WORKDIR /usr
 RUN wget http://repo1.maven.org/maven2/org/liquigraph/liquigraph-cli/3.0.2/liquigraph-cli-3.0.2-bin.tar.gz
 RUN tar xzf liquigraph-cli-3.0.2-bin.tar.gz
 RUN export PATH=$PATH:/usr/liquigraph-cli
+COPY migrate.sh /usr/liquigraph-cli/
+
 WORKDIR /usr/liquigraph-cli
---
+++ migrate.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if [ -z "$NEO_HOST" ]; then
+    echo "NEO_HOST environment variable should be set"
+    exit 1
+fi
+
+if [ -z "$NEO_PORT" ]; then
+    echo "NEO_PORT environment variable should be set"
+    exit 1
+fi
+
+echo $NEO_PASSWORD > pass.txt
+
+if [ -z "$NEO_MIG_CONTEXT" ]; then
+    echo "Run neo migration with default context: common"
+    NEO_MIG_CONTEXT=common
+else
+    echo "Run neo migration with contexts: $NEO_MIG_CONTEXT"
+fi
+
+./liquigraph.sh --changelog /usr/scripts/changelog.xml \
+    --graph-db-uri jdbc:neo4j:$NEO_HOST:$NEO_PORT/ \
+    --execution-contexts $NEO_MIG_CONTEXT \
+    --dry-run-output-directory /tmp \
+    --username $NEO_USERNAME --password < pass.txt
+
+echo "\n Running follow migrations:"
+cat /tmp/output.cypher
+
+./liquigraph.sh --changelog /usr/scripts/changelog.xml \
+    --graph-db-uri jdbc:neo4j:$NEO_HOST:$NEO_PORT/ \
+    --execution-contexts $NEO_MIG_CONTEXT \
+    --username $NEO_USERNAME --password < pass.txt
